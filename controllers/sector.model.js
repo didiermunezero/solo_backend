c7ded008175bfe1f54c60226ddea6715c3b3810b
Didier MUNEZERO <didiermunezer38@gmail.com>
2020-12-04 11:56:48 +0200

    adding get and create controllers to sectors

--- controllers/sector.model.js
+++ controllers/sector.model.js
@@ -13,8 +13,45 @@ exports.create = async (req, res) => {
       message: validator.error.details[0].message,
     });
   }
-  const exists = await Sectors.findOne(name, req.body.name);
+  const exists = await Sectors.findOne({
+    name: req.body.name,
+    district: req.body.district,
+  });
   if (exists) {
     res.status(400).send({ message: "Sector exists already" });
   }
+
+  const create_one = new Sectors(req.body);
+
+  await create_one
+    .save()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log("Error Occurred");
+      res.status(500).send({
+        message: err.message || "Error Occurred",
+      });
+    });
 };
+
+exports.getAll()=(req,res)=>{
+    await Sectors.find()
+    .then((data)=>{
+        res.status(200).send(data);
+    })
+    .catch((err)=>{
+        res.status(400).send({"message":"Error Occurred"})
+    })
+}
+
+exports.findByDistrict=(req,res)=>{
+    await Sectors.find({district: req.params.district})
+    .then(data=>{
+        res.status(200).send(data)
+    })
+    .then((err)=>{
+        res.status(400).send({"message":"Error Occurred"})
+    })
+}
