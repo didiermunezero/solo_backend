57ebe97462b600f826aec9fb25163c9dfbf10bf1
didiermunezer38 <didiermunezer38@gmail.com>
2020-12-06 20:48:50 +0200

    adding districts router

---
+++ routes/districts.router.js
@@ -0,0 +1,19 @@
+module.exports = (app) => {
+    const districts = require("../controllers/sector.controller.js");
+  
+    // Create a new districts
+    app.post("/districts", districts.create);
+  
+    // Retrieve all districtss
+    app.get("/districts", districts.findAll);
+  
+    // Retrieve a single districts by id
+    app.get("/districts/:id", districts.findOne);
+  
+    // Update a districts with id
+    app.get("/districts/district/:district", districts.findByDistrict);
+  
+    // Delete a districts by id
+    app.delete("/districts/:id", districts.delete);
+  };
+  
